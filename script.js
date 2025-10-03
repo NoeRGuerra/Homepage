1f3790aac1f7be3f8716bdfb31beac5cc12fcb88
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-10-03 05:18:37 -0400

    feat: add function to make thumbnail preview a random color

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createPosts(num) {
         const postContainer = document.createElement('div');
         postContainer.className = "post-container"
         const postImage = document.createElement('img');
+        postImage.style.backgroundColor = getRandomColor()
         const postContentContainer = document.createElement('div');
         const titleContainer = document.createElement('div');
         titleContainer.className = "project-title";
@@ -15,11 +16,19 @@ function createPosts(num) {
         linkIcon.classList.add('fa-solid', 'fa-up-right-from-square');
         titleContainer.append(heading, githubIcon, linkIcon);
         const postText = document.createElement('p');
+        postText.className = "post-text";
         postText.textContent = "Short description of the project. Just a couple lines will do."
         postContentContainer.append(titleContainer, postText);
         postContainer.append(postImage, postContentContainer);
         mainContainer.append(postContainer);
     }
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 createPosts(6);
